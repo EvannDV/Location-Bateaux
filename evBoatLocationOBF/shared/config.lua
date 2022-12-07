e918b873bf42df1d7db6226ec11fca1188341d6c
Evann Dv <111243677+EvannDV@users.noreply.github.com>
2022-12-07 22:38:09 +0100

    Add files via upload

---
+++ evBoatLocationOBF/shared/config.lua
@@ -0,0 +1,31 @@
+--------------------- by evann™ ---------------------------
+Config = {}
+evMenu = {}
+--------------------- by evann™ ---------------------------
+
+
+
+
+Config = {
+    posX = -735.06719970703,          --> position du marker pour ouvrir le menu
+    posY = -1315.6391601563,
+    posZ = 5.0003800392151,
+    
+    posXSpawn = -725.88781738281,      --> position du bateaux quand il spawn
+    posYSpawn = -1328.1405029297,
+    posZSpawn = -0.47481319308281,
+    posHSpawn = 235.4302978515625,
+    pedH = 48.22262954711914
+}
+
+evMenu = {                                                          --> Rajoutez ici ce que vous voulez (oubliez pas les virgules)       PS : si ca ne spawn pas c que le vehicule n'est pas sur votre serv :)
+    {name = "dinghy2", label = 'Hors-Bord', price = 200},
+    {name = "tropic", label = 'Tropic de Luxe', price = 1000},
+}
+
+
+
+
+
+
+
